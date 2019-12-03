243e40b26e7ec8e1983f8dbf916903f29d9f98d8
Stephen Cook <stephen.naturalaffinity@gmail.com>
2019-12-03 00:35:15 -0500

    layout is coming along nicely wow!

---
+++ components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="border-t border-indigo-500 bg-gray-900">
+    <div class="h-32">
+
+    </div>
+    <div class="bg-black py-3 px-4 flex justify-center items-center text-center text-white text-xs font-semibold">
+      <span>Built in 
+        <a class="text-indigo-300 hover:text-indigo-400 font-bold" href="https://cincinnatiusa.com/">Cincinnati, Ohio 📍</a> with
+        <a class="text-indigo-300 hover:text-indigo-400 font-bold" href="https://tailwindcss.com/">Tailwind</a> &
+        <a class="text-indigo-300 hover:text-indigo-400 font-bold" href="https://nuxtjs.org/">NuxtJS</a>, hosted on
+        <a class="text-indigo-300 hover:text-indigo-400 font-bold" href="https://www.netlify.com/">Netlify</a> ❤️️
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-
+  <section class="flex justify-center h-screen">
+    <div class="my-container mt-56">
+      <div class="px-4 w-full sm:w-2/3 lg:w-1/2">
+        <div class="border-b border-2 border-white w-1/6"></div>
+        <span class="block pt-8 text-white text-5xl sm:text-6xl font-semibold leading-none">Full Stack Developer, Breakfast Enthusiast.</span>
+        <p class="sm:w-3/4 pt-8 text-indigo-300 font-semibold sm:text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt aliquam voluptas rerum, sit iure obcaecati magni est quibusdam impedit porro temporibus nulla voluptatum molestias aut distinctio vel nisi alias quas.</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
