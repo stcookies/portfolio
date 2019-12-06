15219f05e520614ee53f6f6e3cf6ba176d3e3c2d
Stephen Cook <stephen.naturalaffinity@gmail.com>
2019-12-06 00:35:08 -0500

    card icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="my-container mx-auto">
     <div class="xl:max-w-6xl flex mt-56">
-      <div class="w-full sm:w-3/5">
+      <div class="w-full">
         <div class="border-b border-2 border-white w-1/6"></div>
         <span class="block pt-8 text-white text-4xl sm:text-6xl font-semibold leading-none">Full Stack Developer, Breakfast Enthusiast.</span>
         <div class="hidden xl:block">
-          <p class="sm:text-xl pt-8 text-indigo-300 font-semibold">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt aliquam voluptas rerum, sit iure obcaecati magni est quibusdam impedit porro temporibus nulla voluptatum molestias aut distinctio vel nisi alias quas.</p>
+          <p class="sm:text-xl max-w-xl pt-8 text-indigo-300 font-semibold">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt aliquam voluptas rerum, sit iure obcaecati magni est quibusdam impedit porro temporibus nulla voluptatum molestias aut distinctio vel nisi alias quas.</p>
           <div class="pt-12">
             <a class="btn btn-primary mr-4" href="#">My Work</a>
             <a class="btn btn-secondary" href="#">Contact</a>
@@ -23,40 +23,53 @@
         <a class="btn btn-secondary mt-4 sm:mt-0" href="#">Contact</a>
       </div>
     </div>
-    <div class="flex flex-wrap my-24 xl:-mx-3">
-      <div class="w-full py-3 xl:py-0 xl:w-1/3 xl:px-3">
-        <div class="relative flex justify-center py-12 bg-gray-100 rounded-lg shadow-xl">
-          <div class="absolute -top-12 rounded-full bg-white border-2 border-indigo-500 shadow-xl h-24 w-24">
+    <div class="flex flex-wrap my-20 sm:my-40 xl:-mx-3">
+
+      <div class="w-full py-10 xl:py-0 xl:w-1/3 xl:px-3">
+        <div class="relative flex justify-center px-4 py-12 bg-gray-100 rounded shadow-xl">
+          <div class="absolute -top-12 flex items-center justify-center rounded-full bg-white border-2 border-indigo-500 shadow-xl h-24 w-24">
+            <svg class="h-10 w-10 fill-current text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path class="heroicon-ui" d="M20.59 12l-3.3-3.3a1 1 0 1 1 1.42-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.42-1.4l3.3-3.3zM3.4 12l3.3 3.3a1 1 0 0 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 0 1 1.42 1.4L3.4 12zm7.56 8.24a1 1 0 0 1-1.94-.48l4-16a1 1 0 1 1 1.94.48l-4 16z"/>
+            </svg>
           </div>
-          <div class="flex flex-col items-center px-5 pt-2">
-            <span class="font-bold text-gray-800 uppercase text-xl">UI Development</span>
-            <p class="pt-4 font-semibold text-center text-indigo-500">I have a strong passion for crafting elegant and performant user interfaces with the latest UI technologies.</p>
-            <span class="pt-6 text-gray-600 text-center uppercase font-semibold text-xs">Vue / Nuxt / React / HTML5 / CSS</span>
+          <div class="flex flex-col items-center text-center pt-4">
+            <span class="font-bold text-gray-800 uppercase text-base xl:text-xl">Front-End Development</span>
+            <p class="pt-4 font-semibold text-center text-indigo-500">I have a strong passion for crafting elegant and performant user interfaces with the latest technologies.</p>
+            <span class="pt-10 text-gray-600 text-center uppercase font-semibold text-xs">Vue / Nuxt / React / HTML5 / CSS3</span>
           </div>
         </div>
       </div>
-      <div class="w-full py-3 xl:py-0 xl:w-1/3 xl:px-3">
-        <div class="relative flex justify-center py-12 bg-gray-100 rounded-lg shadow-xl">
-          <div class="absolute -top-12 self-center rounded-full bg-white border-2 border-indigo-500 shadow-xl h-24 w-24">
+
+      <div class="w-full py-10 xl:py-0 xl:w-1/3 xl:px-3">
+        <div class="relative flex justify-center px-4 py-12 bg-gray-100 rounded shadow-xl">
+          <div class="absolute -top-12 flex items-center justify-center rounded-full bg-white border-2 border-indigo-500 shadow-xl h-24 w-24">
+            <svg class="h-10 w-10 fill-current text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path class="heroicon-ui" d="M9 4.58V4c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v.58a8 8 0 0 1 1.92 1.11l.5-.29a2 2 0 0 1 2.74.73l1 1.74a2 2 0 0 1-.73 2.73l-.5.29a8.06 8.06 0 0 1 0 2.22l.5.3a2 2 0 0 1 .73 2.72l-1 1.74a2 2 0 0 1-2.73.73l-.5-.3A8 8 0 0 1 15 19.43V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.58a8 8 0 0 1-1.92-1.11l-.5.29a2 2 0 0 1-2.74-.73l-1-1.74a2 2 0 0 1 .73-2.73l.5-.29a8.06 8.06 0 0 1 0-2.22l-.5-.3a2 2 0 0 1-.73-2.72l1-1.74a2 2 0 0 1 2.73-.73l.5.3A8 8 0 0 1 9 4.57zM7.88 7.64l-.54.51-1.77-1.02-1 1.74 1.76 1.01-.17.73a6.02 6.02 0 0 0 0 2.78l.17.73-1.76 1.01 1 1.74 1.77-1.02.54.51a6 6 0 0 0 2.4 1.4l.72.2V20h2v-2.04l.71-.2a6 6 0 0 0 2.41-1.4l.54-.51 1.77 1.02 1-1.74-1.76-1.01.17-.73a6.02 6.02 0 0 0 0-2.78l-.17-.73 1.76-1.01-1-1.74-1.77 1.02-.54-.51a6 6 0 0 0-2.4-1.4l-.72-.2V4h-2v2.04l-.71.2a6 6 0 0 0-2.41 1.4zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
+            </svg>
           </div>
-          <div class="flex flex-col items-center px-5 pt-2">
+          <div class="flex flex-col items-center text-center pt-4">
             <span class="font-bold text-gray-800 uppercase text-xl">Back-End Development</span>
-            <p class="pt-4 font-semibold text-center text-indigo-500">I have a strong passion for crafting elegant and performant user interfaces with the latest UI technologies.</p>
-            <span class="pt-6 text-gray-600 text-center uppercase font-semibold text-xs">Node / Express / PHP / Laravel</span>
+            <p class="pt-4 font-semibold text-indigo-500">I have a strong passion for crafting elegant and performant user interfaces with the latest UI technologies.</p>
+            <span class="pt-10 text-gray-600 text-center uppercase font-semibold text-xs">Node / Express / Mocha / MongoDB</span>
           </div>
         </div>
       </div>
-      <div class="w-full py-3 xl:py-0 xl:w-1/3 xl:px-3">
-        <div class="relative flex justify-center py-12 bg-gray-100 rounded-lg shadow-xl">
-          <div class="absolute -top-12 self-center rounded-full bg-white border-2 border-indigo-500 shadow-xl h-24 w-24">
+
+      <div class="w-full py-10 xl:py-0 xl:w-1/3 xl:px-3">
+        <div class="relative flex justify-center px-4 py-12 bg-gray-100 rounded shadow-xl">
+          <div class="absolute -top-12 flex items-center justify-center rounded-full bg-white border-2 border-indigo-500 shadow-xl h-24 w-24">
+            <svg class="h-10 w-10 fill-current text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path class="heroicon-ui" d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 1 0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42 0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z"/>
+            </svg>
           </div>
-          <div class="flex flex-col items-center px-5 pt-2">
-            <span class="font-bold text-gray-800 uppercase text-xl">Nacho Slop Development</span>
+          <div class="flex flex-col items-center text-center pt-4">
+            <span class="font-bold text-gray-800 uppercase text-xl">Well Tested Code</span>
             <p class="pt-4 font-semibold text-center text-indigo-500">I have a strong passion for crafting elegant and performant user interfaces with the latest UI technologies.</p>
-            <span class="pt-6 text-gray-600 text-center uppercase font-semibold text-xs">Node / Express / PHP / Laravel</span>
+            <span class="pt-10 text-gray-600 text-center uppercase font-semibold text-xs">Node / Express / PHP / Laravel</span>
           </div>
         </div>
       </div>
+
     </div>
   </section>
 </template>
@@ -64,6 +77,22 @@
 <script>
 export default {
   components: {
+  },
+  data() {
+    return {
+      skillCards: [
+        {
+          name: 'Front-End Development',
+          body: 'I have a strong passion for crafting elegant and performant user interfaces with the latest technologies.',
+          technologies: 'JavaScript / Vue / Nuxt / React / HTML5 / CSS3'
+        },
+        {
+          name: 'Back-End Development',
+          body: 'I have a strong passion for crafting elegant and performant user interfaces with the latest technologies.',
+          technologies: 'JavaScript / Vue / Nuxt / React / HTML5 / CSS3'
+        }
+      ]
+    }
   }
 }
 </script>
