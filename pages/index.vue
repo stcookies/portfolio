f80be25b5993ddb22860f849d2ca499943d26cef
Stephen Cook <stephen.naturalaffinity@gmail.com>
2019-12-07 01:37:58 -0500

    card changes and responsive fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,95 +1,77 @@
 <template>
-  <section class="my-container mx-auto">
-    <div class="xl:max-w-6xl flex mt-56">
-      <div class="w-full">
-        <div class="border-b border-2 border-white w-1/6"></div>
-        <span class="block pt-8 text-white text-4xl sm:text-6xl font-semibold leading-none">Full Stack Developer, Breakfast Enthusiast.</span>
-        <div class="hidden xl:block">
-          <p class="sm:text-xl max-w-xl pt-8 text-indigo-300 font-semibold">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt aliquam voluptas rerum, sit iure obcaecati magni est quibusdam impedit porro temporibus nulla voluptatum molestias aut distinctio vel nisi alias quas.</p>
-          <div class="pt-12">
-            <a class="btn btn-primary mr-4" href="#">My Work</a>
-            <a class="btn btn-secondary" href="#">Contact</a>
+  <div>
+    <section class="my-container mx-auto">
+      <div class="xl:max-w-6xl flex sm:items-center lg:items-start mt-32 sm:mt-56">
+        <div class="w-full sm:w-2/3 lg:w-3/4 sm:pr-4">
+          <div class="border-b border-2 border-white w-1/6"></div>
+          <span class="block pt-8 text-white text-4xl sm:text-6xl font-semibold leading-none">Full Stack Developer, Breakfast Enthusiast.</span>
+          <div class="hidden xl:block">
+            <p class="sm:text-xl max-w-xl pt-8 text-indigo-300 font-semibold">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt aliquam voluptas rerum, sit iure obcaecati magni est quibusdam impedit porro temporibus nulla voluptatum molestias aut distinctio vel nisi alias quas.</p>
+            <div class="pt-12">
+              <a class="btn btn-primary mr-4" href="#">My Work</a>
+              <a class="btn btn-secondary" href="#">Contact</a>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center hidden sm:block">
+          <img class="h-auto bg-gray-900 rounded-full shadow-xl" src="~/assets/img/avatar.png" alt="Stephen Cook Avatar">
+        </div>
+      </div>
+      <div class="xl:hidden">
+        <p class="pt-8 text-indigo-300 font-semibold text-lg sm:text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt aliquam voluptas rerum, sit iure obcaecati magni est quibusdam impedit porro temporibus nulla voluptatum molestias aut distinctio vel nisi alias quas.</p>
+        <div class="pt-8">
+          <a class="btn btn-primary mr-4" href="#">My Work</a>
+          <a class="btn btn-secondary mt-4 sm:mt-0" href="#">Contact</a>
+        </div>
+      </div>
+    </section>
+    <section class="bg-gray-700">
+      <div class="my-container mx-auto">
+        <div class="flex flex-wrap my-20 sm:my-64 xl:-mx-3">
+          <div v-for="(skill, index) in skillCards" :key="index" class="py-8 xl:py-10 xl:w-1/3 xl:px-3 xl:-mt-32">
+            <div class="relative flex justify-center px-4 py-16 bg-white rounded shadow-xl">
+              <div class="absolute -top-12 flex items-center justify-center rounded-full bg-indigo-600 shadow-2xl h-24 w-24">
+                <img class="h-10 w-10" :src="require('~/assets/img/' + skill.icon + '.svg')">
+              </div>
+              <div class="flex flex-col items-center text-center pt-4">
+                <span class="font-bold text-gray-800 text-xl">{{ skill.name }}</span>
+                <p class="py-8 text-gray-700 font-semibold">{{ skill.body }}</p>
+                <span class="text-indigo-500 text-center uppercase font-semibold text-xs">{{ skill.technologies }}</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="flex items-center hidden sm:block">
-        <img class="h-auto bg-gray-900 rounded-full shadow-xl" src="~/assets/img/avatar.png" alt="Stephen Cook Avatar">
-      </div>
-    </div>
-    <div class="xl:hidden">
-      <p class="pt-8 text-indigo-300 font-semibold text-lg sm:text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt aliquam voluptas rerum, sit iure obcaecati magni est quibusdam impedit porro temporibus nulla voluptatum molestias aut distinctio vel nisi alias quas.</p>
-      <div class="pt-8">
-        <a class="btn btn-primary mr-4" href="#">My Work</a>
-        <a class="btn btn-secondary mt-4 sm:mt-0" href="#">Contact</a>
-      </div>
-    </div>
-    <div class="flex flex-wrap my-20 sm:my-40 xl:-mx-3">
-
-      <div class="w-full py-10 xl:py-0 xl:w-1/3 xl:px-3">
-        <div class="relative flex justify-center px-4 py-12 bg-gray-100 rounded shadow-xl">
-          <div class="absolute -top-12 flex items-center justify-center rounded-full bg-white border-2 border-indigo-500 shadow-xl h-24 w-24">
-            <svg class="h-10 w-10 fill-current text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path class="heroicon-ui" d="M20.59 12l-3.3-3.3a1 1 0 1 1 1.42-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.42-1.4l3.3-3.3zM3.4 12l3.3 3.3a1 1 0 0 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 0 1 1.42 1.4L3.4 12zm7.56 8.24a1 1 0 0 1-1.94-.48l4-16a1 1 0 1 1 1.94.48l-4 16z"/>
-            </svg>
-          </div>
-          <div class="flex flex-col items-center text-center pt-4">
-            <span class="font-bold text-gray-800 uppercase text-base xl:text-xl">Front-End Development</span>
-            <p class="pt-4 font-semibold text-center text-indigo-500">I have a strong passion for crafting elegant and performant user interfaces with the latest technologies.</p>
-            <span class="pt-10 text-gray-600 text-center uppercase font-semibold text-xs">Vue / Nuxt / React / HTML5 / CSS3</span>
-          </div>
-        </div>
-      </div>
-
-      <div class="w-full py-10 xl:py-0 xl:w-1/3 xl:px-3">
-        <div class="relative flex justify-center px-4 py-12 bg-gray-100 rounded shadow-xl">
-          <div class="absolute -top-12 flex items-center justify-center rounded-full bg-white border-2 border-indigo-500 shadow-xl h-24 w-24">
-            <svg class="h-10 w-10 fill-current text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path class="heroicon-ui" d="M9 4.58V4c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v.58a8 8 0 0 1 1.92 1.11l.5-.29a2 2 0 0 1 2.74.73l1 1.74a2 2 0 0 1-.73 2.73l-.5.29a8.06 8.06 0 0 1 0 2.22l.5.3a2 2 0 0 1 .73 2.72l-1 1.74a2 2 0 0 1-2.73.73l-.5-.3A8 8 0 0 1 15 19.43V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.58a8 8 0 0 1-1.92-1.11l-.5.29a2 2 0 0 1-2.74-.73l-1-1.74a2 2 0 0 1 .73-2.73l.5-.29a8.06 8.06 0 0 1 0-2.22l-.5-.3a2 2 0 0 1-.73-2.72l1-1.74a2 2 0 0 1 2.73-.73l.5.3A8 8 0 0 1 9 4.57zM7.88 7.64l-.54.51-1.77-1.02-1 1.74 1.76 1.01-.17.73a6.02 6.02 0 0 0 0 2.78l.17.73-1.76 1.01 1 1.74 1.77-1.02.54.51a6 6 0 0 0 2.4 1.4l.72.2V20h2v-2.04l.71-.2a6 6 0 0 0 2.41-1.4l.54-.51 1.77 1.02 1-1.74-1.76-1.01.17-.73a6.02 6.02 0 0 0 0-2.78l-.17-.73 1.76-1.01-1-1.74-1.77 1.02-.54-.51a6 6 0 0 0-2.4-1.4l-.72-.2V4h-2v2.04l-.71.2a6 6 0 0 0-2.41 1.4zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
-            </svg>
-          </div>
-          <div class="flex flex-col items-center text-center pt-4">
-            <span class="font-bold text-gray-800 uppercase text-xl">Back-End Development</span>
-            <p class="pt-4 font-semibold text-indigo-500">I have a strong passion for crafting elegant and performant user interfaces with the latest UI technologies.</p>
-            <span class="pt-10 text-gray-600 text-center uppercase font-semibold text-xs">Node / Express / Mocha / MongoDB</span>
-          </div>
-        </div>
-      </div>
-
-      <div class="w-full py-10 xl:py-0 xl:w-1/3 xl:px-3">
-        <div class="relative flex justify-center px-4 py-12 bg-gray-100 rounded shadow-xl">
-          <div class="absolute -top-12 flex items-center justify-center rounded-full bg-white border-2 border-indigo-500 shadow-xl h-24 w-24">
-            <svg class="h-10 w-10 fill-current text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path class="heroicon-ui" d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 1 0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42 0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z"/>
-            </svg>
-          </div>
-          <div class="flex flex-col items-center text-center pt-4">
-            <span class="font-bold text-gray-800 uppercase text-xl">Well Tested Code</span>
-            <p class="pt-4 font-semibold text-center text-indigo-500">I have a strong passion for crafting elegant and performant user interfaces with the latest UI technologies.</p>
-            <span class="pt-10 text-gray-600 text-center uppercase font-semibold text-xs">Node / Express / PHP / Laravel</span>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
+import backgroundUrl from '~/assets/img/circuit-board.svg';
 export default {
   components: {
   },
   data() {
     return {
+      backgroundUrl,
       skillCards: [
         {
+          icon: 'code',
           name: 'Front-End Development',
-          body: 'I have a strong passion for crafting elegant and performant user interfaces with the latest technologies.',
+          body: 'I have a strong passion for crafting elegant, responsive, and performant user interfaces with the latest technologies.',
           technologies: 'JavaScript / Vue / Nuxt / React / HTML5 / CSS3'
         },
         {
+          icon: 'cog',
           name: 'Back-End Development',
-          body: 'I have a strong passion for crafting elegant and performant user interfaces with the latest technologies.',
-          technologies: 'JavaScript / Vue / Nuxt / React / HTML5 / CSS3'
+          body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia quibusdam voluptates sapiente porro corporis.',
+          technologies: 'Node / Express / Rails / MongoDB / MySQL'
+        },
+        {
+          icon: 'heart',
+          name: 'Well Tested Code',
+          body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia quibusdam voluptates sapiente porro.',
+          technologies: 'Mocha / Chai / RSpec'
         }
       ]
     }
