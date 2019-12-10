93468a69e5c3ab6309018b5b5eb1b48e9d541f8f
Stephen Cook <stephen.naturalaffinity@gmail.com>
2019-12-10 01:34:54 -0500

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,17 @@
 <template>
   <footer class="border-t border-indigo-500 bg-gray-900">
-    <div class="my-container mx-auto flex items-center bg-gray-900 py-32">
-      <span class="text-white text-4xl">Stephen Cook</span>
+    <div class="py-32 my-container mx-auto flex flex-col">
+      <span class="pt-1/5 sm:pt-0 text-white text-xl">Stephen Cook</span>
+      <span class="text-white text-xs">© Copyright 2019</span>
+      <div class="pt-5 sm:pt-0 mt-10 text-white font-semibold sm:flex">
+        <a href="#" class="block pr-4 hover:text-indigo-300">About Me</a>
+        <span class="hidden sm:block sm:px-1 cursor-default">/</span>
+        <a href="#" class="block py-3 sm:py-0 sm:px-4 hover:text-indigo-300">My Work</a>
+        <span class="hidden sm:block sm:px-1 cursor-default">/</span>
+        <a href="#" class="block sm:px-4 hover:text-indigo-300">Contact</a>
+      </div>
     </div>
-    <div class="bg-black py-3 px-4 flex justify-center items-center text-center text-white text-xs font-semibold">
+    <div class="bg-black py-2 px-4 flex justify-center items-center text-center text-white text-xs font-semibold">
       <span>Built in 
         <a class="text-indigo-400 hover:text-indigo-300 font-bold" href="https://cincinnatiusa.com/">Cincinnati, OH 📍</a> with
         <a class="text-indigo-400 hover:text-indigo-300 font-bold" href="https://tailwindcss.com/">Tailwind CSS</a> &
