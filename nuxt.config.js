a620d6ea479be82af1a26d944f87d591199f892b
Stephen Cook <stephen.naturalaffinity@gmail.com>
2020-01-23 10:44:52 -0500

    another netlify form test

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || 'Stephen Cook | Front-End Developer',
+    title: process.env.npm_package_name || 'Stephen Cook / Web Developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -52,9 +52,5 @@ export default {
     */
     extend (config, ctx) {
     }
-  },
-  server: {
-    port: 3000,
-    host: '0.0.0.0'
   }
 }
